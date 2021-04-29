ced3d5721a2dee48e5f277341c426329de40ac88
Mukesh <mukesh.easternts@gmail.com>
2021-04-29 07:42:30 +0530

    Composer Package for meta data

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,20 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
+    $document = YamlFrontMatter::parseFile(resource_path('posts/my-fourth-post.html'));
+
+    ddd($document->title);
     // return Post::find('my-first-post');
-    $posts = Post::findAll();
-    return view('posts', [
-        'posts' => $posts
-    ]);
+    // $posts = Post::findAll();
+    // return view('posts', [
+    //     'posts' => $posts
+    // ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
     $post = Post::find($slug);
-    
+
     return view('post', [
         'post' => $post
     ]);
-})->where('post','[A-z_\-]+'); // Allowed only dash and underscore in route
+})->where('post', '[A-z_\-]+'); // Allowed only dash and underscore in route
