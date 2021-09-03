7e450e780445b5f66ee98260b0c60e7a1d9a850f
Mukesh <mukesh.easternts@gmail.com>
2021-09-03 08:24:48 +0530

    Newsletter

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,29 @@ use App\Http\Controllers\SessionsController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('post');
 
+Route::post('newsletter', function () {
+
+    request()->validate(['email' => 'required|email']);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' =>  config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember('77bdd4d225', [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages(["email" => "This email could not be added to our newsletter."]);;
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter');
+});
+
 
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
