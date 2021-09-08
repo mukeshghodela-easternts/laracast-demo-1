30957a507e902072022e39465ea2605bbe7a28d3
Mukesh <mukesh.easternts@gmail.com>
2021-09-08 08:05:11 +0530

    PHP class for configuration mailchimp

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Services\Newsletter;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,21 +21,9 @@ use App\Services\Newsletter;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('post');
 
-Route::post('newsletter', function (Newsletter $newsletter) {
-
-    request()->validate(['email' => 'required|email']);
-
-    try {
-        $newsletter->subscribe(request('email'));
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages(["email" => "This email could not be added to our newsletter."]);;
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter');
-});
-
-
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
